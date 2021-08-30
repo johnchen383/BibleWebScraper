65129b9bc07befe8aa6299129c0ed323f37ef6cb
John Chen <john.bm.chen@gmail.com>
2021-08-30 23:46:27 +1200

    finished woohoooo. calvinism > arminianism

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from fpdf import FPDF
-# from fontTools.ttLib import TTFont
-# font = TTFont('Helvetica.ttf')
 
 book = input("What book? ")
-chapter = input("Enter range. E.g. '1:1-2:4' OR '1' OR '2-3 ' ")
+chapter = input("Enter range. E.g. '1:1-2:4' OR '1' OR '2-3' ")
 translation = input("What translation? [ESV, NIV, CSB, ASV, HWP] ")
+
+header = book.title() + " " + chapter + " [" + translation.upper() + "]\n\n"
 
 URL = "https://www.biblegateway.com/passage/?search=" + book + "+" + chapter + "&version=" + translation + "&interface=print"
 page = requests.get(URL)
@@ -14,7 +14,6 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find("div", class_="text-html")
-verses = results.find_all("span", class_="text")
 
 #Get rid of subheadings
 head = results.find_all("h3")
@@ -39,24 +38,57 @@ for s in smallCaps:
 
 text=""
 
-# Formatting 'Selah', psalms, proverbs, psalm 119, psalms heading, song of songs headings, OT passage in NT,
+paragraphs = soup.find_all(["p", "h4"])
 
-for v in verses:
-    text=text+v.text+" "
+for p in paragraphs:   
+    verses = p.find_all("span", class_="text")
+    
+    if len(verses) == 0:
+        continue
+
+    for v in verses:
+        text+=v.text+" "
+        
+        if p.get('class') == None:
+            continue
+
+        if "line" in p.get('class'):
+            text+="\n"
+
+    if p.get('class') == None:
+        text+="\n\n"
+        continue
+
+    if "line" in p.get('class'):
+        text+="\n"
+    else:
+        text+="\n\n"
 
 
+#Format 'Selah'
+if book.lower() == "psalm":
+    text = text.replace("Selah", "        Selah")
 
 print(text)
+
+
+#scrape copyright notice
+cpyright = soup.find("div", class_="copyright-table")
+# print(cpyright.prettify())
+
 
 #Create PDF
 pdf = FPDF(orientation = 'P', unit = 'mm', format = 'A4')
 pdf.set_margins(30, 20, 30)
 pdf.add_page()
 pdf.add_font('Helvetica', '', 'Helvetica.ttf', uni=True)
-pdf.set_font('Helvetica', style = '', size = 12)
 pdf.set_text_color(0, 0, 0)
 
-pdf.write(10, text)
+pdf.set_font('Helvetica', style = '', size = 24)
+pdf.write(7, header)
+
+pdf.set_font('Helvetica', style = '', size = 12)
+pdf.write(7, text)
 
 #Output
 pdf.output('Manuscript.pdf')
